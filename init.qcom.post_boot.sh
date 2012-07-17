4d335b5a33d82569026cfc578278b9d9305cc7f1
Nikez <nikez141@gmail.com>
2012-07-17 20:49:54 +0200

    init.qcom.post_boot: Governor: smartassv2
    Change-Id: I9e03e0aa555fae0c42b28a110a9e941a20c1f324

--- init.qcom.post_boot.sh
+++ init.qcom.post_boot.sh
@@ -13,7 +13,7 @@ case "$target" in
         echo 245000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
         echo 998400 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
         # Cpu Governor
-        echo "ondemand" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+        echo "smartassv2" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
         echo 40000 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
         # CpuGovernorService
         chown system /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
